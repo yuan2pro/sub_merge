b860dd6a3b90e524ed39966516ae6936ff6e167b
freeman <ypjalt@gmail.com>
2025-01-17 20:31:29 +0800

    fix

--- merge.py
+++ merge.py
@@ -3,6 +3,7 @@ import yaml
 
 def merge_proxies(directory, output_file):
     all_proxies = []
+    seen_names = set()  # 用于存储已经遇到的代理名称
 
     # 遍历目录下的所有 .yaml 文件
     for filename in os.listdir(directory):
@@ -11,7 +12,10 @@ def merge_proxies(directory, output_file):
             with open(filepath, 'r', encoding='utf-8') as file:
                 data = yaml.safe_load(file)
                 if 'proxies' in data:
-                    all_proxies.extend(data['proxies'])
+                    for proxy in data['proxies']:
+                        if proxy['name'] not in seen_names:  # 检查代理名称是否已经存在
+                            all_proxies.append(proxy)
+                            seen_names.add(proxy['name'])  # 将新的代理名称添加到集合中
             os.remove(filepath)
 
     # 生成新的 .yaml 文件
